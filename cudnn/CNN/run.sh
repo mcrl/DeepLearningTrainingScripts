6568250567d2cdfd205667bfce95fb1a07c1e757
hyungmo kim <hyungmo@aces.snu.ac.kr>
2019-07-30 12:51:03 +0900

    add experiment bash script
    Former-commit-id: ac52932cb06d6ac5a2606dcd9fe88599bf1c1cec
    Former-commit-id: fe50085bb423cc678797d64389f34562f117ae87

---
+++ cudnn/CNN/run.sh
@@ -0,0 +1,68 @@
+#!/bin/bash
+
+####################################################
+#networks=vgg resnet densenet inception
+networks=vgg resnet densenet
+
+batch_size=32
+iteration=10
+
+data_path=/home/data/imagenet_10240.data
+label_path=/home/data/imagenet_10240.label
+
+#host_list=("c0" "c1" "c2" "c3" "c4" "c5" "c6" "c8")
+host_list=("c0" "c2" "c4" "c5" "c6" "c8")
+dev_list=("0" "1" "2" "3")
+
+####################################################
+
+host_name() {
+  local num_nodes=$1
+  local acc=${host_list[0]}
+  for (( i = 1 ; i < ${num_nodes} ; i++ )) ; do
+    acc="${acc},${host_list[${i}]}"
+  done
+  echo ${acc}
+}
+
+dev_name() {
+  local num_devs=$1
+  local acc=${dev_list[0]}
+  for (( i = 1 ; i < ${num_devs} ; i++ )) ; do
+    acc="${acc},${dev_list[${i}]}"
+  done
+  echo ${acc}
+}
+
+run_test() {
+  local num_nodes=$1
+  local num_gpus=$2
+  local net=$3
+
+  local total_batch_size=$((num_nodes * num_gpus * batch_size))
+  local hosts=$(host_name ${num_nodes})
+  local devs=$(dev_name ${num_gpus})
+
+  echo "[${net}] ${num_nodes} nodes x ${num_gpus} gpus"
+  
+  mpirun \
+    --host ${hosts} -x CUDA_VISIBLE_DEVICES=${devs} \
+    ./${net} ${total_batch_size} ${iteration} ${data_path} ${label_path} \
+    model/${net}_${total_batch_size}_${iteration} > \
+    log/${net}_${num_nodes}_${num_gpus}
+}
+
+####################################################
+
+mkdir -p model
+mkdir -p log
+
+for net in ${networks} ; do
+make ${net}
+run_test 1 1 ${net}
+run_test 1 2 ${net}
+run_test 1 4 ${net}
+run_test 2 4 ${net}
+run_test 4 4 ${net}
+#run_test 8 4 ${net}
+done
