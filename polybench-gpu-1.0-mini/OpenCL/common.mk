cc4e28f9f6335dd92a1649ba58ab618d2c1f6a73
Heehoon Kim <heehoon@aces.snu.ac.kr>
2021-12-03 10:21:50 +0900

    Set flag for soff test

--- polybench-gpu-1.0-mini/OpenCL/common.mk
+++ polybench-gpu-1.0-mini/OpenCL/common.mk
@@ -1,11 +1,22 @@
-OpenCL_SDK=/home/scott/NVIDIA_GPU_Computing_SDK
-INCLUDE=-I${OpenCL_SDK}/OpenCL/common/inc
-LIBPATH=-L${OpenCL_SDK}/OpenCL/common/lib -L${OpenCL_SDK}/shared/lib
-LIB=-lOpenCL -lm
-BENCH_FLAGS=-DWITH_SOURCE='"kernel0.cl"'
-BENCH_FLAGS=-DWITH_BINARY='"kernel0.cl.aocx"'
+# GPU
+#BENCH_FLAGS=-DWITH_SOURCE='"kernel0.cl"'
+
+# Intel FPGA
+#COMPILE_FLAGS=$(shell aocl compile-config)
+#LINK_FLAGS=$(shell aocl link-config)
+COMPILE_FLAGS=-I/opt/intelFPGA_pro/quartus_19.2.0b57/hld/host/include
+LINK_FLAGS=-L/opt/intelFPGA_pro/quartus_19.2.0b57/hld/host/linux64/lib -lOpenCL -lm
+#BENCH_FLAGS=-DINTEL -DWITH_BINARY='"kernel0.aocx"'
+
+# soff
+#COMPILE_FLAGS=-I/usr/local/cuda/include
+#LINK_FLAGS=-L/usr/local/cuda/lib64 -lOpenCL
+BENCH_FLAGS=-DSOFF -DWITH_BINARY='"kernel0.cl.sfb"'
+
+BENCH_FLAGS+=-DMEASURE -DMEASURE_TIME_THRESHOLD=30 -DMEASURE_IDLE_WATT=355
+
 all:
-	gcc -O3 ${INCLUDE} ${LIBPATH} ${LIB} ${CFILES} -o ${EXECUTABLE} ${BENCH_FLAGS}
+	gcc -O3 ${CFILES} -o ${EXECUTABLE} ${BENCH_FLAGS} ${COMPILE_FLAGS} ${LINK_FLAGS}
 
 clean:
 	rm -f *~ *.exe
