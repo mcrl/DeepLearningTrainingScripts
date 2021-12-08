f14617ecd617a8de3d7345ec96017bca6060c0a3
Heehoon Kim <heehoon@aces.snu.ac.kr>
2021-12-09 01:03:20 +0900

    Run one more time for validation (SYR2K)

--- polybench-gpu-1.0-mini/OpenCL/SYR2K/syr2k.c
+++ polybench-gpu-1.0-mini/OpenCL/SYR2K/syr2k.c
@@ -314,6 +314,11 @@ int main(void)
 	cl_load_prog();
 
 	cl_launch_kernel();
+#ifdef MEASURE
+  measure_disable();
+	cl_mem_init(A, B, C);
+	cl_launch_kernel();
+#endif
 
 	errcode = clEnqueueReadBuffer(clCommandQue, c_mem_obj, CL_TRUE, 0, N*M*sizeof(DATA_TYPE), C_outputFromGpu, 0, NULL, NULL);
 	if(errcode != CL_SUCCESS) printf("Error in reading GPU mem\n");
