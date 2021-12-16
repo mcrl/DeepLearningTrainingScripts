65ac9a39fad93dc247c0b945c95f8d579609e7db
Heehoon Kim <heehoon@aces.snu.ac.kr>
2021-12-16 23:47:55 +0900

    Integration functionality

--- hioki3334/hioki3334.py
+++ hioki3334/hioki3334.py
@@ -37,3 +37,45 @@ class hioki3334:
     self.sendrecv(':rs232:answ on')
     # turn off header to make parsing easier
     self.sendrecv(':head off')
+
+  def integrate_reset(self):
+    self.sendrecv(':integ:stat stop')
+    self.sendrecv(':integ:stat reset')
+
+  def integrate_start(self):
+    self.sendrecv(':integ:stat start')
+
+  def integrate_stop(self):
+    self.sendrecv(':integ:stat stop')
+
+  def measure_wh_time(self):
+    wh, time = self.sendrecv(':meas? wh,time')
+    wh = float(wh)
+    h, m, s = time.split(',')
+    time = int(h) * 3600 + int(m) * 60 + int(s)
+    return wh, time
+
+def main():
+  parser = argparse.ArgumentParser()
+  parser.add_argument('-i', action='store_true', help='Initialization.')
+  parser.add_argument('-s', action='store_true', help='Start integration.')
+  parser.add_argument('-e', action='store_true', help='End integration.')
+  parser.add_argument('-r', action='store_true', help='Reset integration.')
+  parser.add_argument('-m', action='store_true', help='Measure wh and time.')
+  args = parser.parse_args()
+
+  dev = hioki3334()
+  if args.i:
+    dev.init()
+  if args.s:
+    dev.integrate_start()
+  if args.e:
+    dev.integrate_stop()
+  if args.r:
+    dev.integrate_reset()
+  if args.m:
+    wh, time = dev.measure_wh_time()
+    print(f'{wh:.6f},{time}')
+
+if __name__ == '__main__':
+  main()
